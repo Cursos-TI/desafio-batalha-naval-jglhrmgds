46534b762312d3d5c33f6e73618852026a815a59
Joao Guilherme <jguimg9128@gmail.com>
2025-03-06 10:37:43 -0300

    Desafio Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -94,6 +94,16 @@ int main() {
     {
         posicionar_navio(tabuleiro, 4, 1, 3, 'V');
     }
+
+    if (pode_posicionar(tabuleiro, 1, 4, 3, 'H'))
+    {
+        posicionar_navio(tabuleiro, 1, 4, 3, 'H');
+    }
+    
+    if (pode_posicionar(tabuleiro, 6, 3, 3, 'V'))
+    {
+        posicionar_navio(tabuleiro, 6, 3, 3, 'V');
+    }
     
 
     //Exibindo o tabuleiro
@@ -117,16 +127,6 @@ int main() {
 
     return 0;
 }
-
-// Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
